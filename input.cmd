8189086abbb3aa03721b43136ca0a2e647454591
Aaron Dotter <aaron.dotter@gmail.com>
2015-01-28 14:11:34 +1100

    adding info to input.cmd

--- input.cmd
+++ input.cmd
@@ -1,2 +1,8 @@
 bc_table.list
 cmd
+
+
+first line gives the filename where the BC tables are specified.  See existing example.
+second line is the suffix which will be added to the isochrone filename to create the 
+new isochrone file with magnitudes included.  basic format of the new file is similar to 
+the original isochrone file except that magnitudes are written at lower resolution.
