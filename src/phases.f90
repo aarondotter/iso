540ae31ba6b234643e3e3e3d474ca32f1ece13c1
Aaron Dotter <aaron.dotter@gmail.com>
2016-01-21 10:02:32 +1100

    attempting to fix issue #6 on the misidentification of WR and post-AGB

--- src/phases.f90
+++ src/phases.f90
@@ -6,33 +6,32 @@ module phases
   implicit none
 
 
-  !     isochrone primary EEPs
-  !     t% EEP( 1) = PreMS(t,5.6d0,1)
-  !     t% EEP( 2) = ZAMS(t,10)
-  !     t% EEP( 3) = TAMS(t,1d-1,t% EEP(ieep-1))
-  !     t% EEP( 4) = TAMS(t,1d-4,t% EEP(ieep-1))
-  !     t% EEP( 5) = RGBTip(t,t% EEP(ieep-1))
-  !     t% EEP( 6) = ZAHB(t,t% EEP(ieep-1))
-  !     t% EEP( 7) = TAHB(t,1d-1,t% EEP(ieep-1))
-  !     t% EEP( 8) = TAHB(t,1d-4,t% EEP(ieep-1))
-  !     for low-mass stars
-  !     t% EEP( 9) = TPAGB(t,t% EEP(ieep-1))
-  !     t% EEP(10) = PostAGB(t,t% EEP(ieep-1))
-  !     t% EEP(11) = WDCS(t, t% EEP(ieep-1))
-  !     for high-mass stars
-  !     t% EEP( 9) = CarbonBurn(t,t% EEP(ieep-1))
+!    t% EEP( 1) = PreMS_Tc(t,5.0d0,1); if(check(t,ieep)) return; ieep=ieep+1
+!    t% EEP( 2) = ZAMS(t,10); if(check(t,ieep)) return; ieep=ieep+1
+!    t% EEP( 3) = TAMS(t,3.5d-1,t% EEP(ieep-1)+inc); if(check(t,ieep)) return; ieep=ieep+1
+!    t% EEP( 4) = TAMS(t,1d-12,t% EEP(ieep-1)+inc); if(check(t,ieep)) return; ieep=ieep+1
+!    t% EEP( 5) = RGBTip(t,t% EEP(ieep-1)+inc); if(check(t,ieep)) return; ieep=ieep+1
+!    t% EEP( 6) = ZAHB(t,t% EEP(ieep-1)+inc); if(check(t,ieep)) return; ieep=ieep+1
+!    t% EEP( 7) = TAHB(t,1d-4,t% EEP(ieep-1)+inc); if(check(t,ieep)) return; ieep=ieep+1
+! if(t% star_type == star_low_mass)then
+!    t% EEP( 8) = TPAGB(t,t% EEP(ieep-1)+inc); if(check(t,ieep)) return; ieep=ieep+1
+!    t% EEP( 9) = PostAGB(t,t% EEP(ieep-1)+inc); if(check(t,ieep)) return; ieep=ieep+1
+!    t% EEP(10) = WDCS(t, t% EEP(ieep-1)+inc)
+! elseif(t% star_type == star_high_mass)then
+!    t% EEP( 8) = CarbonBurn(t,t% EEP(ieep-1)+inc)
+! endif
 
-  !     FSPS phase information
-  !     0: main sequence
-  !     1: sub giant branch
-  !     2: red giant branch
-  !     3: red horizontal branch / red clump (core He burning)
-  !     4: early asymptotic giant branch
-  !     5: thermally-pulsating AGB (composition sets C-rich vs. O-rich)
-  !     6: Post-AGB
-  !     7: blue stragglers
-  !     8: blue horizontal branch
-  !     9: Wolf-Rayet (composition sets WC vs. WN)
+!     FSPS phase information
+!     0: main sequence
+!     1: sub giant branch
+!     2: red giant branch
+!     3: red horizontal branch / red clump (core He burning)
+!     4: early asymptotic giant branch
+!     5: thermally-pulsating AGB (composition sets C-rich vs. O-rich)
+!     6: Post-AGB
+!     7: blue stragglers
+!     8: blue horizontal branch
+!     9: Wolf-Rayet (composition sets WC vs. WN)
 
   real(dp), parameter :: pre_MS = -1d0
   real(dp), parameter :: MS = 0d0
@@ -58,6 +57,8 @@ contains
     n = t% ntrack
 
     allocate(t% phase(n))
+
+    t% phase = undefined
 
     do i=1,n
        next_eep = 0
@@ -87,7 +88,8 @@ contains
           phase = undefined   
        end select
 
-       if(t% tr(i_logTe,i) >= 4d0 .and. t% tr(i_surfH,i) <= 3d-1) phase = Wolf_Rayet
+       if(t% tr(i_logTe,i) >= 4d0 .and. t% tr(i_surfH,i) <= 3d-1 .and. &
+            t% star_type == star_high_mass) phase = Wolf_Rayet
 
        t% phase(i) = phase
     enddo
