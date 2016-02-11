1f302611c297b133761bbd47557bfe76c03099b0
Aaron Dotter <aaron.dotter@gmail.com>
2016-02-11 13:58:39 +1100

    iso_interp_age works

--- src/iso_interp_age.f90
+++ src/iso_interp_age.f90
@@ -1,4 +1,4 @@
-program iso_interp
+program iso_interp_age
 
   !MESA modules
   use const_def, only: dp
@@ -12,219 +12,219 @@ program iso_interp
 
   implicit none
 
-  type(isochrone_set), allocatable :: s(:)
-  type(isochrone_set) :: t
-  real(dp), allocatable :: Z_div_H(:)
-  real(dp) :: new_Z_div_H
-  integer :: ierr, n, i
-  logical :: do_colors
+  type(isochrone_set) :: old, new
+  integer, parameter :: single=0, range=1, list=2
+  integer :: ierr
   
-  call read_interp_input(ierr)
-  if(ierr/=0) stop ' iso_interp: failed reading input list'
-
-  do i=1,n
-     call read_isochrone_file(s(i),ierr)
-     if(ierr/=0)  stop ' iso_interp: failed in read_isochrone_file'
-  enddo
-
-  call consistency_check(s,ierr)
-  if(ierr/=0) stop ' iso_interp: failed consistency check(s)'
+  call read_interp_age_input(ierr)
+  if(ierr/=0) stop ' iso_interp_age: failed reading input list'
 
   !if all checks pass, then proceed with interpolation
-  call interpolate(n,Z_div_H,new_Z_div_H,s,t,ierr)
+  call interpolate_age(old,new,ierr)
 
   !write the new isochrone to file
-  if(ierr==0) then
-     call write_isochrones_to_file(t)
-     if(do_colors) call write_cmds_to_file(t)
-  endif
+  if(ierr==0) call write_isochrones_to_file(new)
 
 contains
 
 
-  subroutine read_interp_input(ierr)
+  subroutine read_interp_age_input(ierr)
     integer, intent(out) :: ierr
-    integer :: io, i
-    character(len=file_path) :: iso_list, arg
-    character(len=file_path) :: bc_list, cstar_list
+    integer :: age_scale, age_type, number_of_ages, i
+    character(len=file_path) :: input, output, arg
+    real(dp) :: age_range_lo, age_range_hi, delta
+    real(dp), allocatable :: age(:)
     ierr = 0
-    do_colors = .false.
 
-    if(command_argument_count() < 3) then
+    if(command_argument_count() < 4) then
        ierr=-1
-       write(0,*) 'iso_interp            '
+       write(0,*) 'iso_interp_age        '
        write(0,*) '   usage:             '
-       write(0,*) ' ./iso_interp [list] [Z/H] [output] [Av]'
-       write(0,*) '  Av is optional;                       '
-       write(0,*) '   will generate a .cmd file if included'
+       write(0,*) ' ./iso_interp_age [input] [output] [age_scale] [age_type] [age(s)]'
+       write(0,*) '     [input] = input file name   '
+       write(0,*) '     [output] = output file name (can be same as input)'
+       write(0,*) '     [age_scale] = integer, 0 for linear or 1 for log10'
+       write(0,*) '     [age_type]  = integer, 0 for single, 1 for range, 2 for list'
+       write(0,*) '     [age(s)] = real               '
        return
-    else if(command_argument_count() > 3) then
-       do_colors=.true.
-       t% cmd_suffix = 'cmd'
-       bc_list = 'bc_table.list'
-       cstar_list = 'cstar_table.list'
-       call iso_color_init(bc_list,.false.,cstar_list,ierr)
-       if(ierr/=0) then 
-          write(0,'(a)') ' problem reading BC_table_list = ', trim(bc_list)
-          do_colors=.false.
+    endif
+
+    call get_command_argument(1,input)
+    call get_command_argument(2,output)
+    call get_command_argument(3,arg)
+    read(arg,*) age_scale
+    call get_command_argument(4,arg)
+    read(arg,*) age_type
+    if(age_type==single)then
+       number_of_ages = 1
+       allocate(age(1))
+       call get_command_argument(5,arg)
+       read(arg,*) age(1)
+    else
+       call get_command_argument(5,arg)
+       read(arg,*) number_of_ages
+
+       allocate(age(number_of_ages))
+       if(age_type==range) then
+          call get_command_argument(6,arg)
+          read(arg,*) age_range_lo
+          call get_command_argument(7,arg)
+          read(arg,*) age_range_hi
+          delta = (age_range_hi - age_range_lo)/real(number_of_ages-1,kind=dp)
+          do i=1,number_of_ages
+             age(i) = age_range_lo + delta*real(i-1,kind=dp)
+          enddo
+       else if(age_type==list)then
+          do i=1,number_of_ages
+             call get_command_argument(5+i,arg)
+             read(arg,*) age(i)
+          enddo
        endif
     endif
 
-    call get_command_argument(1,iso_list)
-    call get_command_argument(2,arg)
-    read(arg,*) new_Z_div_H
-    call get_command_argument(3,t% filename)
-    if(do_colors)then
-       call get_command_argument(4, arg)
-       read(arg,*) t% Av
-       t% Rv = 3.1
-    endif
+    !now set up the isochrone types...
+    old% filename = trim(adjustl(input))
+    new% filename = trim(adjustl(output))
 
-    io=alloc_iounit(ierr)
-    open(io,file=trim(iso_list),action='read',status='old')
-    read(io,*) n
-    allocate(s(n),Z_div_H(n))
-    do i=1,n
-       read(io,'(f5.2,1x,a)') Z_div_H(i), s(i)% filename
+    call read_isochrone_file(old,ierr)
+    if(ierr/=0) return    
+
+    new% version_number = old% version_number
+    new% number_of_isochrones = number_of_ages
+
+    allocate(new% iso(new% number_of_isochrones))
+    new% iso(:)% has_phase = old% iso(1)% has_phase
+
+
+    do i=1,new% number_of_isochrones
+       new% iso(i)% age_scale = age_scale
+       if(new% iso(i)% age_scale == age_scale_linear)then
+          new% iso(i)% age = log10(age(i))
+       elseif(new% iso(i)% age_scale == age_scale_log10)then
+          new% iso(i)% age = age(i)
+       endif
     enddo
-    close(io)
-    call free_iounit(io)
-  end subroutine read_interp_input
 
-
-  subroutine consistency_check(s,ierr)
-    type(isochrone_set), intent(in) :: s(:)
-    integer, intent(out) :: ierr
-    integer :: n, i
-
-    ierr = 0
-
-    n=size(s)
-
-    if(any(s(:)% number_of_isochrones /= s(1)% number_of_isochrones))then
-       write(0,*) ' iso_interp : failed consistency check - inconsistent number of isochrones '
-       ierr=-1
-       do i=1,n
-          write(0,*) trim(s(i)% filename), s(i)% version_number
+    if(.false.)then
+       do i=1,new% number_of_isochrones
+          write(*,*) new% iso(i)% age
        enddo
+       
+       do i=1,old% number_of_isochrones
+          write(*,*) old% iso(i)% age
+       enddo
+       
+       ierr=-1
     endif
 
-    if(any(s(:)% version_number /= s(1)% version_number))then
-       write(0,*) ' iso_interp : failed consistency check - inconsistent version number '
-       ierr=-1
-       do i=1,n
-          write(0,*) trim(s(i)% filename), s(i)% number_of_isochrones
-       enddo
-    endif
-
-    !more checks here as needed
-
-  end subroutine consistency_check
+  end subroutine read_interp_age_input
 
 
-  subroutine interpolate(n,Z,newZ,s,t,ierr)
-    integer, intent(in) :: n
-    real(dp), intent(in) :: Z(n), newZ
-    type(isochrone_set), intent(in) :: s(n)
-    type(isochrone_set), intent(inout) :: t
+  subroutine interpolate_age(old,new,ierr)
+    type(isochrone_set), intent(in) :: old
+    type(isochrone_set), intent(inout) :: new
     integer, intent(out) :: ierr
-    integer :: i, loc, lo, hi, order
+    integer :: i, j, n, loc, lo, hi, order
     loc=0; order=0; lo=0; hi=0
 
     ierr = 0
 
-    do i=1,n-1
-       if(newZ >= Z(i) .and. newZ < Z(i+1)) then
-          loc=i
-          exit
-       endif
-    enddo
+    !we have two sets of ages, new and old
 
-    if(loc==0)then
+    !first make sure they are compatible
+    if(minval(new% iso(:)% age) < minval(old% iso(:)% age))then
+       write(0,*) ' iso_interp_age error: new age minimum is less than old age minimum '
        ierr=-1
-       write(0,*) ' iso_interp: no extrapolation! '
-       return
+    else if(maxval(new% iso(:)% age) > maxval(old% iso(:)% age))then
+       write(0,*) ' iso_interp_age error: new age maximum is greater than old age maximum '
+       ierr=-1
     endif
 
-    !this results in either cubic (order=4) or linear (order=2)
-    if(loc==n-1 .or. loc==1)then
-       lo=loc
-       hi=loc+1
-    else
-       lo=loc-1
-       hi=loc+2
-    endif
+    if(ierr/=0) return
 
-    order = hi - lo + 1  ! either 4, 3, or 2
+    n = old% number_of_isochrones
 
-    print *, Z(lo:hi), newZ
+    do i = 1, new% number_of_isochrones
 
-    t% number_of_isochrones =s(lo)% number_of_isochrones
-    t% version_number = s(lo)% version_number
-    allocate(t% iso(t% number_of_isochrones))
-    t% iso(:)% age_scale = s(lo)% iso(:)% age_scale
-    t% iso(:)% has_phase = s(lo)% iso(:)% has_phase
-    t% iso(:)% age       = s(lo)% iso(:)% age
-    t% iso(:)% ncol      = s(lo)% iso(:)% ncol
-    do i = 1, t% number_of_isochrones
-       allocate(t% iso(i)% cols(t% iso(i)% ncol))
-       t% iso(i)% cols(:)% name = s(lo)% iso(i)% cols(:)% name
-       t% iso(i)% cols(:)% type = s(lo)% iso(i)% cols(:)% type
-       t% iso(i)% cols(:)% loc = s(lo)% iso(i)% cols(:)% loc
-    enddo
-    call do_interp(order,Z(lo:hi),newZ,s(lo:hi),t)
-  end subroutine interpolate
-
-
-  subroutine do_interp(n,Z,newZ,s,t)
-    integer, intent(in) :: n
-    real(dp), intent(in) :: Z(n), newZ
-    type(isochrone_set), intent(in) :: s(n)
-    type(isochrone_set), intent(inout) :: t
-    integer :: i, j, neep, eeps_lo(n), eeps_hi(n)
-    integer :: eep, eep_lo, eep_hi, ioff(n), ncol
-
-    do i=1,n
-       write(*,*) trim(s(i)% filename), Z(i)
-    enddo
-
-    do i=1,t% number_of_isochrones
-       do j=1,n
-          eeps_lo(j) = s(j)% iso(i)% eep(1)
-          neep=s(j)% iso(i)% neep
-          eeps_hi(j) = s(j)% iso(i)% eep(neep)
-       enddo
-       eep_lo = maxval(eeps_lo)
-       eep_hi = minval(eeps_hi)
-       t% iso(i)% neep = eep_hi - eep_lo + 1
-       neep = t% iso(i)% neep
-       ncol = t% iso(i)% ncol
-       allocate(t% iso(i)% eep(neep), t% iso(i)% data(ncol,neep))
-       if(t% iso(i)% has_phase) allocate(t% iso(i)% phase(neep))      
-       t% iso(i)% data = 0d0
-
-       !the EEP offsets are for alignment: we only interpolate at constant EEP number
-       do j=1,n
-          ioff(j) = eep_lo - s(j)% iso(i)% eep(1)  
-       enddo
-
-       !loop over the EEPs
-!$omp parallel do private(eep)
-       do eep=1,neep
-          t% iso(i)% eep(eep) = eep_lo + eep - 1
-          t% iso(i)% phase(eep) = s(1)% iso(i)% phase(eep+ioff(1))
-          if(n==2)then 
-             t% iso(i)% data(:,eep) = linear(ncol, Z, newZ, s(1)% iso(i)% data(:,eep+ioff(1)), s(2)% iso(i)% data(:,eep+ioff(2)))
-          else if(n==3) then
-             t% iso(i)% data(:,eep) = quadratic(ncol, Z, newZ, s(1)% iso(i)% data(:,eep+ioff(1)), &
-                  s(2)% iso(i)% data(:,eep+ioff(2)), s(3)% iso(i)% data(:, eep+ioff(3)))   
-          else if(n==4) then 
-             t% iso(i)% data(:,eep) = cubic( ncol, Z, newZ, s(1)% iso(i)% data(:,eep+ioff(1)), &
-                  s(2)% iso(i)% data(:,eep+ioff(2)), s(3)% iso(i)% data(:, eep+ioff(3)), s(4)% iso(i)% data(:,eep+ioff(4)) )
+       do j = 1, n-1
+          if(new% iso(i)% age > old% iso(j)% age .and. new% iso(i)% age <= old% iso(j+1)% age)then
+             loc=j
+             exit
           endif
        enddo
-!$omp end parallel do
+
+       !this results in either cubic (order=4) or linear (order=2)
+       if(loc==n-1 .or. loc==1)then
+          lo=loc
+          hi=loc+1
+       else
+          lo=loc-1
+          hi=loc+2
+       endif
+       
+       order = hi - lo + 1  ! either 4 or 2 
+
+       new% iso(i)% has_phase = old% iso(lo)% has_phase
+       new% iso(i)% ncol = old% iso(lo)% ncol
+       allocate(new% iso(i)% cols(new% iso(i)% ncol))
+       do j = 1, new% number_of_isochrones
+          new% iso(i)% cols(:)% name = old% iso(lo)% cols(:)% name
+          new% iso(i)% cols(:)% type = old% iso(lo)% cols(:)% type
+          new% iso(i)% cols(:)% loc  = old% iso(lo)% cols(:)% loc
+       enddo
+       call do_interp(order,old% iso(lo:hi),new% iso(i))
     enddo
+  end subroutine interpolate_age
+
+
+  subroutine do_interp(n,s,t)
+    integer, intent(in) :: n
+    type(isochrone), intent(in) :: s(n)
+    type(isochrone), intent(inout) :: t
+    integer :: j, neep, eeps_lo(n), eeps_hi(n)
+    integer :: eep, eep_lo, eep_hi, ioff(n), ncol
+    real(dp) :: ages(n), new_age
+
+    new_age = t% age
+
+    do j=1,n
+       eeps_lo(j) = s(j)% eep(1)
+       neep=s(j)% neep
+       eeps_hi(j) = s(j)% eep(neep)
+       ages(j) = s(j)% age
+    enddo
+
+    eep_lo = maxval(eeps_lo)
+    eep_hi = minval(eeps_hi)
+    t% neep = eep_hi - eep_lo + 1
+    neep = t% neep
+    ncol = t% ncol
+    allocate(t% eep(neep), t% data(ncol,neep))
+    if(t% has_phase) allocate(t% phase(neep))
+    t% data = 0d0
+
+    !the EEP offsets are for alignment: we only interpolate at constant EEP number
+    do j=1,n
+       ioff(j) = eep_lo - s(j)% eep(1)
+    enddo
+
+    !loop over the EEPs
+!$omp parallel do private(eep)
+    do eep=1,neep
+       t% eep(eep) = eep_lo + eep - 1
+       t% phase(eep) = s(1)% phase(eep+ioff(1))
+       if(n==2)then 
+          t% data(:,eep) = linear(ncol, ages, new_age, s(1)% data(:,eep+ioff(1)), s(2)% data(:,eep+ioff(2)))
+       else if(n==3) then
+          t% data(:,eep) = quadratic(ncol, ages, new_age, s(1)% data(:,eep+ioff(1)), &
+               s(2)% data(:,eep+ioff(2)), s(3)% data(:, eep+ioff(3)))   
+       else if(n==4) then 
+          t% data(:,eep) = cubic( ncol, ages, new_age, s(1)% data(:,eep+ioff(1)), &
+               s(2)% data(:,eep+ioff(2)), s(3)% data(:, eep+ioff(3)), s(4)% data(:,eep+ioff(4)) )
+       endif
+    enddo
+!$omp end parallel do
+
   end subroutine do_interp
 
 
@@ -271,4 +271,4 @@ contains
   end function cubic
 
 
-end program iso_interp
+end program iso_interp_age
